572082389cf987bc8a62863836335d4a526890de
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-09-06 12:56:21 +0800

    project updated to newest moralis functions

--- static/logic.js
+++ static/logic.js
@@ -1,10 +1,10 @@
 Moralis.initialize(""); //application ID
 Moralis.serverURL = ""; //ServerURL
 
-const chainToQuery = "bsc testnet"
-const tokenAddress ="0x7eaaf8070ec57474af786f687ca3c940710d1c8d"
-
-const web3 = new Web3(window.ethereum);
+/* Valid values for chain in https://docs.moralis.io/moralis-server/transactions-and-balances/intro */
+const chainToQuery = "bsc testnet" 
+// Dan Token address 
+const tokenAddress ="0x7eaaf8070ec57474af786f687ca3c940710d1c8d" //
 
 async function login(){
     Moralis.Web3.authenticate().then(function(user){
@@ -51,59 +51,32 @@ function transferNative(){
     const balance = document.getElementById("nativeBalance").value;
     const transferValue = document.getElementById("nativeAmount").value;
     const transferTo = document.getElementById("nativeTransferTo").value;
-    (transferValue <= balance && transferValue <= 999) ? _transferNative(transferTo, transferValue): displayMessage("01","Your balance is not enough or surpases integer limit");
+    (transferValue <= balance) ? _transferNative(transferTo, transferValue): displayMessage("01","Your balance is not enough");
 }
 
 function transferToken(){
     const balance = document.getElementById("tokenBalance").value;
     const transferValue = document.getElementById("tokenAmount").value;
     const transferTo = document.getElementById("tokenTransferTo").value;
-    (transferValue <= balance && transferValue <= 999) ? _transferToken(transferTo, transferValue): displayMessage("01","Your balance is not enough or surpases integer limit");
+    (transferValue <= balance) ? _transferToken(transferTo, transferValue): displayMessage("01","Your balance is not enough");
 }
 
-//Blockchain has no decimals, we have to transform everything to integers
-//Blockchain EVM works in hexadecimal values
-
 async function _transferNative(transferTo, transferValue){
-    const blockchainValue = codeValue(transferValue,16);
-    const transactionParameters = {
-        to: transferTo,
-        from: ethereum.selectedAddress,
-        value: blockchainValue
-    }
-    const txtHash = await ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [transactionParameters]
-    }) 
-    displayMessage("00",txtHash)
+    const options = {type: "native", amount: Moralis.Units.ETH(transferValue), receiver: transferTo}
+    let result = await Moralis.transfer(options)
+    setControls()
+    displayMessage("00","Transaction processed at " + result.transactionHash)
 }
 
 async function _transferToken(transferTo, transferValue){
-    const blockchainValue = codeValue(transferValue,10);
-    const encodedFunction = web3.eth.abi.encodeFunctionCall({
-        name:"transfer",
-        type:"function",
-        inputs: [{
-            type:"address",
-            name:"_to"},
-            {
-            type:"uint256",
-            name:"_value"
-            }]
-    },[transferTo,blockchainValue]);
-    const transactionParameters = {
-        to: tokenAddress,
-        from: ethereum.selectedAddress,
-        data: encodedFunction
-    }
-    const txtHash = await ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [transactionParameters]
-    }) 
-    displayMessage("00",txtHash)
+    const options = {type: "erc20", 
+                 amount: Moralis.Units.Token(transferValue, "18"), 
+                 receiver: transferTo,
+                 contract_address: tokenAddress}
+    let result = await Moralis.transfer(options)
+    setControls()
+    displayMessage("00","Transaction processed at " + result.transactionHash)
 }
 
-function codeValue(_value,_encoding){
-    const result = parseInt(web3.utils.toWei(_value,"ether")).toString(_encoding)
-    return result;
-}
+
+
